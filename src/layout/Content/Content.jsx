bedad261b946c6bdcff0b0a7b0c9002fbea5517b
Darius Kiuberis <dariuskiuberis@gmail.com>
2019-08-20 20:45:16 +0100

    add content skeleton, buttons and input

--- src/layout/Content/Content.jsx
+++ src/layout/Content/Content.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import List from '../../modules/List/List'
 import './Content.scss'
 
 class Content extends Component {
   render() {
     window.Content = this
-    return <div className="content">TEST</div>
+    return (
+      <div className="content">
+        Content
+        <div className="content__wrapList">
+          <div className="content__listTop">
+            <List />
+          </div>
+          <div className="content__listMiddle">
+            <List />
+          </div>
+          <div className="content__listBottom">
+            <List />
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
---
+++ src/modules/List/List.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './List.scss'
+
+class List extends Component {
+  render() {
+    return <div className="list"> List </div>
+  }
+}
+
+export default List
